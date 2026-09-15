8fe252523aa9ded980549822e44d8067beb65804
lineSence <paralelepipid654@gmail.com>
2026-09-15 14:08:05 +0300

    Добавлена сборка веб-роутеров

---
+++ app/web/routers/__init__.py
@@ -0,0 +1,3 @@
+from . import health, mail, refs, result, training, update, upload, vision
+ROUTERS=(health.router,upload.router,result.router,vision.router,mail.router,refs.router,update.router,training.router)
+__all__=["ROUTERS"]
